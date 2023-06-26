ec21baafd67df55e18aba52167a9bed640e2658a
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-06-26 19:49:15 +0330

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,9 @@
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+student_code = input("Enter your student code : ")
+
+# print(name)
+# print(age)
+# print(student_code)
+
+print(name,age,student_code)
---
+++ 4.py
@@ -0,0 +1,4 @@
+birth = int(input("Sale tavalod khod ra vared konid : "))
+year = 1402
+age = year - birth
+print(age)
