31854c4fa61f55bc717825950fe6a24e91cf39fe
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-07-01 18:35:46 +0330

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,19 @@
+x = int(input("Enter first number : "))
+y = int(input("Enter second number : "))
+
+# if x != y:
+#
+#     if x > y:
+#         print(x, 'Is bigger')
+#     else:
+#         print(y, 'Is bigger')
+# else:
+#     print("x and y are equal")
+
+if x == y:
+    print("x and y are equal")
+else:
+    if x > y:
+        print(x, 'Is bigger')
+    else:
+        print(y, 'Is bigger')
---
+++ 14.py
@@ -0,0 +1,7 @@
+name = "alireza"
+
+# if "ali" in name[0:3]:
+if name[0:3] == "ali":
+    print(True)
+else:
+    print(False)
