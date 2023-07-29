021db42dcfbae55f7a1b465c48a5264039452a3c
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-07-29 20:22:28 +0330

    Add files via upload

---
+++ 63.py
@@ -0,0 +1,16 @@
+def greet(name):
+    print("Hello", name)
+
+
+greet("Farhad")
+
+# def greet(name):
+#     return "Agha" + " " + name
+#
+#
+# print(greet("Reza"))
+
+names = ["alex", "taylor", "james", "tom"]
+
+for i in names:
+    greet(i)
