5fc28346c7dca2a6bf158b4f0e632703848805c4
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-08-05 20:16:34 +0330

    Add files via upload

---
+++ 77.py
@@ -0,0 +1,18 @@
+import math
+
+
+def greet(name):
+    return "Hello " + name
+
+
+# Aliasing
+# ahvalporsi = greet
+# print(ahvalporsi)
+# print(greet)
+
+# -----------------------------
+# jazr = math.sqrt
+# print(jazr(100))
+
+bechap = print
+bechap("salam")
---
+++ 81.py
@@ -0,0 +1,13 @@
+user = "Ali Kamrani"
+
+# msg = """
+# Moshtarak gerami {us} shoma 80% az baste
+# khod ra masraf kardid.
+# """.format(us=user)
+
+msg = f"""
+Moshtarak gerami {user} shoma 80% az baste
+khod ra masraf kardid.
+"""
+
+print(msg)
