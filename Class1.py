d864e1dc4a516b1683c4dbdafba95aa888d6dc42
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-09-02 20:24:56 +0330

    Add files via upload

--- Class1.py
+++ Class1.py
@@ -1,9 +1,13 @@
+import time
+
+
 class SedanCar:
     def __init__(self, hp, color, fuel, gb):
         self.horsepower = hp
         self.color = color
         self.fuel = fuel
         self.gearbox = gb
+        self.rpm = 0
 
     def car_horn(self):
         print("Boogh")
@@ -11,7 +15,15 @@ class SedanCar:
     def car_break(self):
         print("Tormmmmoz")
 
+    def accelerate(self, value):
+        for i in range(value):
+            time.sleep(2)
+            self.rpm += 1000
+            print(self.rpm)
+
 
 # Shey missazim
 samand = SedanCar(113, "white", "petrol", "manual")
 dena = SedanCar(113, "black", "petrol", "automatic")
+
+dena.accelerate(value=3)
